f6decdf89e9412b63f4954806233002fc4582004
Diego Cruz <felipe.cruzm@outlook.com>
2025-09-15 09:24:40 -0500

    initial structure

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { Suspense, lazy } from 'react'
+
+import './App.css'
+
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Login = lazy(() => import('./pages/Login'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+export default App
